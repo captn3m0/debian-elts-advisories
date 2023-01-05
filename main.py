13e00c84a0c81e475f64aed3f0ea17fce98dcdbe
Nemo <me@captnemo.in>
2023-01-05 18:19:31 +0530

    Fix references

--- main.py
+++ main.py
@@ -16,14 +16,17 @@ DEBIAN_CODENAME = {
     "squeeze": "6",
 }
 
+
 def fetch_ela_list():
     url = "https://salsa.debian.org/freexian-team/extended-lts/security-tracker/-/raw/master/data/ELA/list"
     response = urllib.request.urlopen(url)
-    return response.read().decode('utf-8')
+    return response.read().decode("utf-8")
+
 
 def parse_date(s):
     # '15 Jun 2018'
     return datetime.datetime.strptime(s, "%d %b %Y")
+
 
 def get_osv():
     content = fetch_ela_list()
@@ -34,7 +37,7 @@ def get_osv():
         line = line.strip()
         m = re.match(TITLE_REGEX, line)
         if m:
-            if cves and data and len(details)>0:
+            if cves and data and len(details) > 0:
                 yield {
                     "id": data["id"],
                     "modified": parse_date(data["date"]).isoformat("T") + "Z",
@@ -44,25 +47,31 @@ def get_osv():
                             "package": {
                                 "ecosystem": f"Debian:{DEBIAN_CODENAME[r['codename']]}",
                                 "name": r["package"],
-                                "purl": f"pkg:deb/debian/{data['package']}?distro={r['codename']}?repository_url=http%3A%2F%2Fdeb.freexian.com%2Fextended-lts",
+                                "purl": f"pkg:deb/debian/{data['package']}?distro={r['codename']}&repository_url=http%3A%2F%2Fdeb.freexian.com%2Fextended-lts",
                             },
                             "ranges": {
                                 "type": "ECOSYSTEM",
-                                "events": [{
-                                    "fixed": r['version'],
-                                }]
-                            }
+                                "events": [
+                                    {
+                                        "fixed": r["version"],
+                                    }
+                                ],
+                            },
                         }
                         for r in details
                     ],
-                    "database_specific": {
-                        "type": data['type']
-                    },
+                    "database_specific": {"type": data["type"]},
                     "references": [
-                        f"https://deb.freexian.com/extended-lts/tracker/{data['id']}"
+                        {
+                            "type": "ADVISORY",
+                            "url": f"https://deb.freexian.com/extended-lts/tracker/{data['id']}",
+                        }
                     ]
                     + [
-                        f"https://deb.freexian.com/extended-lts/tracker/{cve}"
+                        {
+                            "type": "ADVISORY",
+                            "url": f"https://deb.freexian.com/extended-lts/tracker/{cve}",
+                        }
                         for cve in cves
                     ],
                 }
@@ -76,12 +85,14 @@ def get_osv():
         if m:
             details.append(m.groupdict())
 
+
 def __main__():
     for d in get_osv():
         fn = f"advisories/{d['id']}.json"
         with open(fn, "w") as f:
             print(f"writing to {fn}")
             f.write(json.dumps(d, indent=4, sort_keys=True))
+
 
 if __name__ == "__main__":
     __main__()
